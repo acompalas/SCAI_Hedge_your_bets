a9f1bce4b4ce6162c706cf493e5df9549ebea74d
Anderson Compalas <ajcompalas@gmail.com>
2023-11-08 14:03:12 -0800

    converted csv to dict

--- nba_prediction.py
+++ nba_prediction.py
@@ -3,32 +3,53 @@ import pandas as pd
 import os
 
 # Specify the path to the parent folder ("SCAI_comp")
-parent_folder = '/Users/andersoncompalas/Documents/SCAI_comp'
+read_file = '/Users/andersoncompalas/Documents/SCAI_comp/datasets/nba_games.csv'
 
-# Specify the name of the dataset folder
-dataset_folder = 'datasets'
+# Specify the path for the new CSV file
+write_file = '/Users/andersoncompalas/Documents/SCAI_comp/datasets/games.csv'
 
-# Specify the name of the CSV file
-file_name = 'nba_games.csv'
+# # Construct the full path to the CSV file
+# csv_file_path = os.path.join(parent_folder, dataset_folder, file_name)
 
-# Construct the full path to the CSV file
-csv_file_path = os.path.join(parent_folder, dataset_folder, file_name)
-
-# Initialize an empty dictionary to store the data
-mydict = {}
+# Initialize an empty list to store the data
+data_list = []
 
 # Open the CSV file in read mode
-with open(csv_file_path, 'r') as csv_file:
+with open(read_file, 'r') as csv_file:
     # Create a CSV reader
     csv_reader = csv.reader(csv_file)
     
-    # Iterate through each row in the CSV file and create the dictionary
+    # Read the first row to get the column headers
+    headers = next(csv_reader)
+    
+    # Iterate through each row in the CSV file and create a dictionary for each row
     for row in csv_reader:
-        mydict[row[0]] = row[1]
+        row_dict = {header: value for header, value in zip(headers, row)}
+        data_list.append(row_dict)
 
-# Display the first 5 rows from the dictionary
-for key, value in list(mydict.items())[:5]:
-    print(f'Key: {key}, Value: {value}')
+# Open the new CSV file in write mode
+with open(write_file, 'w', newline='') as new_csv_file:
+    # Define the column headers based on the keys of the dictionaries
+    headers = data_list[0].keys()
+
+    # Create a CSV writer
+    csv_writer = csv.DictWriter(new_csv_file, fieldnames=headers)
+    
+    # Write the headers to the new CSV file
+    csv_writer.writeheader()
+    
+    # Write the data from the list of dictionaries to the new CSV file
+    for row in data_list:
+        csv_writer.writerow(row)
+        
+# Read the CSV file into a Pandas DataFrame
+df = pd.read_csv(write_file)
+
+# Display the first few rows of the DataFrame to view the data
+print(df.head())
+        
+
+
 
 
 
